5cdf7c0785997de8582d10e87ffaa178817ae5f8
ka <todo1@rotarydef.net>
2023-10-20 05:51:49 -0700

    test

--- exists/exists.hpp
+++ exists/exists.hpp
@@ -64,10 +64,11 @@ int tmain_kautil_range_exsits_interface() {
     
     using value_type = uint64_t;
     using offset_type = long;
+    auto step = 10;
     auto data = std::vector<value_type>();{
         for(auto i = 0; i < 100; ++i){
-             data.push_back(i*10+10);
-             data.push_back(data.back()+10);
+             data.push_back(i*step+step);
+             data.push_back(data.back()+step);
         }
     }
     
@@ -76,13 +77,50 @@ int tmain_kautil_range_exsits_interface() {
     auto written = fwrite(data.data(),sizeof(value_type),data.size(),f_ranges);
     fflush(f_ranges);
     {
+        
         auto fd = fileno(f_ranges);
         printf("file size : %ld\n",lseek(fd,0,SEEK_END)-lseek(fd,0,SEEK_SET));
         
         auto pref = file_syscall_16b_pref{.fd=fd};
         auto bt = kautil::algorithm::btree_search{&pref};
+    
+        //test info
+        auto min = data.front();
+        auto max = data.back();
+        srand((uintptr_t)&pref);
         
+//        for(auto i = 0; i < 1000; ++i){
+            auto from=value_type{10},to=value_type{20};
+//            from = (rand()%max+min)/step*step;
+//            to = (rand()%max+min)/step*step;
+            // 4 pattern
+                //exact
+                //contained
+            auto i0 = bt.search(from,false);
+            auto i1 = bt.search(to,false);
+            auto is_exact = [](auto const& i0,auto const& i1)->bool{
+                return
+                         ((!i0.direction)&!bool(i0.nearest_pos%(sizeof(value_type)*2))) 
+                        &((!i1.direction)& bool(i1.nearest_pos%(sizeof(value_type)*2)));
+            };
+            auto is_contained = [](auto const& in)->bool{
+                return 
+                    bool(in.nearest_pos%(sizeof(value_type)*2))&(in.direction<0) 
+                 + !bool(in.nearest_pos%(sizeof(value_type)*2))&(in.direction>0);
+            };
+            auto exact = is_exact(i0,i1);
+            auto contained = is_contained(i0)&is_contained(i1);
+            auto size_check = (sizeof(value_type)==(-i0.nearest_pos+i1.nearest_pos));
+            auto res = bool(exact+contained)&size_check; 
         
+            auto i0_check_buf = value_type(0);
+            auto i1_check_buf = value_type(0);
+            auto i_buf_ptr = &i0_check_buf;
+            pref.read_value(i0.nearest_pos,&(i_buf_ptr=&i0_check_buf));
+            pref.read_value(i1.nearest_pos,&(i_buf_ptr=&i1_check_buf));
+            printf("%d : (from,to){%lld %lld} ansewer{%lld,%lld} \n",res,from,to,i0_check_buf,i1_check_buf);
+            int jjj = 0;
+//        }
         
         
     }
